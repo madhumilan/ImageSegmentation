b8748893a36a1a308da22923bd7e6f0ec2c14cac
Madhu Milan <madhumilan37@gmail.com>
2021-06-25 22:21:24 +0200

    Changing path conventions

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,10 @@ def main():
 		st.write('Choose a different image using the drop down')
 
 		img_num = img_name[6]
-		pred_mask_img = os.path.join(folder_path, "..\\mask_images")
+		pred_mask_img = os.path.join(folder_path, "../mask_images")
 		pred_mask_img = pred_mask_img+"\\pred_mask"+str(img_num)+".png"
 
-		true_mask_img =	 os.path.join(folder_path, "..\\mask_images")
+		true_mask_img =	 os.path.join(folder_path, "../mask_images")
 		true_mask_img = true_mask_img+"\\true_mask"+str(img_num)+".png"
 
 		img = image.load_img(filename, target_size=(224,224,3))
